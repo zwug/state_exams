f45353ca2b4b2c3e6ed0eb383c3c2dcf1c4c6f2b
Kirill Mironov <zwugme@gmail.com>
2015-03-07 13:30:02 +0300

    created live page update

--- app/views/main.js
+++ app/views/main.js
@@ -1,8 +1,16 @@
+require('./less/main.less');
+
 var React = require('react');
 var AppComponent = React.createClass({
     render: function () {
         return (
-            <h1>Hello worldy!</h1>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-5">
+                        <button className="btn btn-danger">lohi</button>
+                    </div>
+                </div>
+            </div>
         );
     }
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,18 @@
 var path = require('path');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 
 module.exports = {
     entry: [
-        'webpack-dev-server/client?http://localhost:3000',
+        'webpack-dev-server/client?http://localhost:8080',
         'webpack/hot/only-dev-server',
         './app/views/main.js'
     ],
     output: {
         path: './app/build',
+        publicPath: "/assets/",
         filename: 'app.js'
     },
     module: {
@@ -19,12 +21,26 @@ module.exports = {
                 test: /\.js$/,
                 loaders: ['react-hot', 'jsx?harmony'],
                 exclude: /node_modules/
+            },
+            {
+                test: /\.less$/,
+               //loader: ExtractTextPlugin.extract("style-loader", "css-loader!autoprefixer!less-loader"),
+                loader: 'style!css!autoprefixer!less',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(svg|eot|ttf|woff)/,
+                loader: 'url'
             }
         ]
     },
     plugins: [
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NoErrorsPlugin(),
-        new HtmlWebpackPlugin({})
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'app/views/app.html'
+        }),
+        new ExtractTextPlugin("app.css")
     ]
 };
