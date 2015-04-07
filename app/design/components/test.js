8aa7829b910d431f8cf49261d7614b315c035d99
Kirill Mironov <zwugme@gmail.com>
2015-04-07 15:43:44 +0300

    code refactoring

--- app/design/components/test.js
+++ app/design/components/test.js
@@ -1,14 +1,11 @@
 const React = require('react');
 let $ = require('jquery');
 
-
-
 const SingleAnswer = require('./single-answer');
 const MultipleAnswer = require('./multiple-answer');
 const ComplianceAnswer = require('./compliance-answer');
 const ConseqAnswer = require('./conseq-answer');
 const AdditionalAnswer = require('./additional-answer');
-
 
 let Test = React.createClass({
 
@@ -24,23 +21,22 @@ let Test = React.createClass({
       }.bind(this)
     });
   },
-
-
+  
   render() {
     let questionList = '';
     if(this.state){
       questionList =
         <form>
-          <SingleAnswer questionData={this.state ? this.state.questionData.singleAnswer[0] : ''} questionNum={1} />
-          <SingleAnswer questionData={this.state ? this.state.questionData.singleAnswer[1] : ''} questionNum={2} />
-          <MultipleAnswer questionData={this.state ? this.state.questionData.multipleAnswer[0] : ''} questionNum={3} />
-          <MultipleAnswer questionData={this.state ? this.state.questionData.multipleAnswer[1] : ''} questionNum={4} />
-          <ComplianceAnswer questionData={this.state ? this.state.questionData.complianceAnswer[0] : ''} questionNum={5} />
-          <ComplianceAnswer questionData={this.state ? this.state.questionData.complianceAnswer[1] : ''} questionNum={6} />
-          <ConseqAnswer questionData={this.state ? this.state.questionData.conseqAnswer[0] : ''} questionNum={7} />
-          <ConseqAnswer questionData={this.state ? this.state.questionData.conseqAnswer[1] : ''} questionNum={8} />
-          <AdditionalAnswer questionData={this.state ? this.state.questionData.additionalAnswer[0] : ''} questionNum={9} />
-          <AdditionalAnswer questionData={this.state ? this.state.questionData.additionalAnswer[1] : ''} questionNum={10} />
+          <SingleAnswer questionData={this.state.questionData.singleAnswer[0] } questionNum={1} />
+          <SingleAnswer questionData={this.state.questionData.singleAnswer[1] } questionNum={2} />
+          <MultipleAnswer questionData={this.state.questionData.multipleAnswer[0] } questionNum={3} />
+          <MultipleAnswer questionData={this.state.questionData.multipleAnswer[1] } questionNum={4} />
+          <ComplianceAnswer questionData={this.state.questionData.complianceAnswer[0] } questionNum={5} />
+          <ComplianceAnswer questionData={this.state.questionData.complianceAnswer[1] } questionNum={6} />
+          <ConseqAnswer questionData={this.state.questionData.conseqAnswer[0] } questionNum={7} />
+          <ConseqAnswer questionData={this.state.questionData.conseqAnswer[1] } questionNum={8} />
+          <AdditionalAnswer questionData={this.state.questionData.additionalAnswer[0] } questionNum={9} />
+          <AdditionalAnswer questionData={this.state.questionData.additionalAnswer[1] } questionNum={10} />
         </form>;
     }
 
